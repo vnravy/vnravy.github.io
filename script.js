60a017568aa1fd1094b03013220074d036def056
vianra <ravnsy@gmail.com>
2024-02-29 12:15:08 +0700

    add pagination

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+function show(shown, hidden, navbar) {
+  // document.getElementById(shown).style.display='block';
+  document.getElementById(navbar).style.display='none';
+  var x = document.getElementById(shown);
+  var y = document.getElementById(hidden);
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+    // if (y.style.display === 'block') {
+      y.style.display = 'none'
+    // }
+  }
+  // else {
+  //   x.style.display = 'none';
+  //   // if (y.style.display === 'none') {
+  //     y.style.display = 'block';
+  //   // }
+  // }
+
+  return false;
+}
+function myFunction() {
+  var x = document.getElementById('myDIV');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
